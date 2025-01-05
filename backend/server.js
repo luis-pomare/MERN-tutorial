96193a5ca51a5965442c9b0c38c65354d14a2a24
Luis Pomare <luispomare92@gmail.com>
2025-01-05 14:31:19 -0500

    Create basic route for root get request

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
+// Create a express app
 const app = express();
 
+app.get('/', (res, req) => {
+  res.json({ mssg: 'wellcome to the app Luis' })
+})
+
 app.listen(4000, () => {
-  console.log('Listening on port 4000')
+  console.log('Listening on port 4000 in your computer')
 })
