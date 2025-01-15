05a52eafd6bb5212c9598506006024bcabf5bcff
Luis Pomare <luispomare92@gmail.com>
2025-01-15 06:33:34 -0500

    Create routes file

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
 require('dotenv').config()
 const express = require('express');
+const workoutRoutes = require('./routes/workouts')
 
 // Create a express app
 const app = express();
 
+
 // Global middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
 
 // Routes
-app.get('/', (req, res) => {
-  res.json({ mssg: 'wellcome to the app Luis' })
-})
+app.use('/api/workouts', workoutRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log('Listening on port 4000 in your computer')
